df3ceb1bddd7cbb5962fc387bf3068af64ea9726
Bakki Mahesh <237y1a04f1@mlritm.ac.in>
2025-04-28 19:48:03 +0530

    i did the converter program using python

---
+++ Converter.py
@@ -0,0 +1,12 @@
+centimeter =float(input("Enter the lenght in centimeter: "))
+
+inches=centimeter/2.54
+feet=centimeter/30.48
+i=centimeter/2.54
+feet=centimeter/30.48
+#cal the length in feet
+print("\n---------------Length Conversion---------------")
+print(f"{centimeter} cm is equal to {inches:.2f} inches")
+print(f"{centimeter} cm is equal to {feet:.2f} inches")
+print(f"{centimeter} cm is equal to {i:.2f} inches")
+print(f"{centimeter} cm is equal to {feet:.2f} feet")
